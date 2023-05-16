6d1d5836b81c9ad5e2879da108e9cdef2f7125f4
Jose Gómez <gomezmorata2@gmail.com>
2023-05-16 12:20:34 +0200

    FastaGen

---
+++ BuscadorDeGen/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        boolean continuar = true;
+
+        while (continuar){
+            System.out.println("1. Introduce la cadena a comprobar");
+            System.out.println("2. Introduce la cadena del gen");
+            System.out.println("3. Comprobar");
+            String resp = leer.nextLine();
+
+            switch(resp){
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+            }
+
+        }
+
+    }
+}
